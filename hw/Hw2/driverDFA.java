a2c0aff12a8d030d8516b8411878ffacbf6e9cdf
Alex Goncalves <agoncalves4141@gmail.com>
2017-04-02 18:59:30 -0400

    Fixing repository structure

--- hw/Hw2/driverDFA.java
+++ hw/Hw2/driverDFA.java
@@ -1,3 +1,17 @@
+/**
+* file: driverDFA.java
+* author: Alexander Goncalves
+* course: CMPT 440
+* assignment: project 1
+* due date: March 20th, 2017
+* version 1
+*
+* driverDFA
+* This class is the driver class for the Man Wolf Goat Cabbage
+* Problem. It creates an object from the ManWolf class and 
+* passes the command line argument to determine it's validity.
+*/
+
 class driverDFA {
   
   /*
