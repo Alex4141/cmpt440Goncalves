437b81723e1c9f2fcbf22564c3007b046b7c79ee
Alex Goncalves <agoncalves4141@gmail.com>
2017-04-25 21:51:03 -0400

    Added man pages command

---
+++ prj/code/cmd/Man.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+class Man extends Command {
+
+	public static final String name = "man";
+
+	public static final int PARAM_LEN = 1;
+	public static final int OPTION_LEN = 0;
+
+	public static String execute(ArrayList<String> param){
+		String command = param.get(0);
+
+		switch(command){
+			case "whoami":
+				return "This command returns the user using the application";
+			case "cp":
+				return "This command copies one file to another directory";
+			case "echo":
+				return "This command returns a given input to the screen";
+			case "head":
+				return "Returns the first 10 or less lines of a file";
+			case "man":
+				return "Returns information about commands in the system";
+			case "rm":
+				return "This commands removes the specified file if it exists";
+			case "touch":
+				return "This command creates a file if it doesn't exist";
+			case "mkdir":
+				return "This command makes a new directory with the name specified";
+			default:
+				return command + " is not a valid command";
+		}
+	}
+}
--- prj/code/cmd/test.java
+++ prj/code/cmd/test.java
@@ -2,5 +2,6 @@ import java.util.ArrayList;
 
 class test{
 	public static void main(String[] args){
+
 	}
 }
